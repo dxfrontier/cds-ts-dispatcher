7672940e219679efc8362c7203085a190a89c382
dragolea <dragolea@yahoo.com>
2023-10-03 10:48:18 +0300

    Create SINGLE_INSTANCE.test.ts

---
+++ test/unit/SINGLE_INSTANCE.test.ts
@@ -0,0 +1,32 @@
+import { Constructable } from '@sap/cds/apis/internal/inference';
+import { connectTest } from '../util/util';
+import { SingleInstanceCapable } from '../../lib';
+import { Request } from '@sap/cds';
+import Constants from '../../lib/util/constants/Constants';
+
+function createCustomerInstance(): Constructable {
+  class Customer {
+    @SingleInstanceCapable()
+    public async doSomething(results: any[], req: Request, isSingleInstance: boolean) {}
+  }
+
+  return Customer;
+}
+
+describe('SINGLE-INSTANCE', () => {
+  describe('@SingleInstanceCapable', () => {
+    test(`It should RETURN : 'true' which means that all methods after this will be marked as Single Instance`, async () => {
+      const newCustomer = (customer: Constructable) => {
+        return new customer();
+      };
+
+      const isSingleInstance: boolean = Reflect.getMetadata(
+        Constants.DECORATOR.SINGLE_INSTANCE_FLAG_KEY,
+        newCustomer(createCustomerInstance()),
+        'doSomething',
+      );
+
+      expect(isSingleInstance).toBe(true);
+    });
+  });
+});
