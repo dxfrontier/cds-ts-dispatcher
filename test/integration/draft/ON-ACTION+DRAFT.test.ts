05f1ceb2fab1e110a8ae8aca48017e89a3e8f635
dragolea <dragolea@yahoo.com>
2023-10-04 14:21:33 +0300

    Create ON-ACTION+DRAFT.test.ts

---
+++ test/integration/draft/ON-ACTION+DRAFT.test.ts
@@ -0,0 +1,41 @@
+import { Request } from '@sap/cds';
+import { Draft, OnBoundAction, OnBoundFunction } from '../../../lib';
+import { MetadataDispatcher } from '../../../lib/util/helpers/MetadataDispatcher';
+import { Constructable } from '@sap/cds/apis/internal/inference';
+
+const testFn = (actionMethod: number) => {};
+
+class Customer {
+  @OnBoundAction(testFn)
+  @Draft()
+  public async BeforeReadMethod(req: Request, next: Function) {}
+
+  @OnBoundFunction(testFn)
+  @Draft()
+  public async BeforeCreateAndDeleteMethod(req: Request, next: Function) {}
+}
+
+const newCustomer = (customer: Constructable) => new customer();
+const decoratorProps = MetadataDispatcher.getMetadataHandlers(newCustomer(Customer));
+
+describe('ON - Draft', () => {
+  describe(`
+  @OnBoundAction(testFn)
+  @Draft()`, () => {
+    it('It should : mark @OnBoundAction(testFn) decorator as "draft"', () => {
+      const found = decoratorProps.filter((item) => item.event === 'BOUND_ACTION')[0];
+
+      expect(found.isDraft).toBe(true);
+    });
+  });
+
+  describe(`
+  @OnBoundFunction(testFn)
+  @Draft()`, () => {
+    it('It should : mark @OnBoundFunction(testFn) decorator as "draft"', () => {
+      const found = decoratorProps.filter((item) => item.event === 'BOUND_FUNC')[0];
+
+      expect(found.isDraft).toBe(true);
+    });
+  });
+});
