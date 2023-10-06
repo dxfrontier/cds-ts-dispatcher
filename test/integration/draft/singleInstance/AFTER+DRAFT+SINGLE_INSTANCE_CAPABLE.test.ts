ca8489fcd561cd971f0e6810028a395775d8f368
dragolea <dragolea@yahoo.com>
2023-10-06 10:08:25 +0300

    Update AFTER+DRAFT+SINGLE_INSTANCE_CAPABLE.test.ts

--- test/integration/draft/singleInstance/AFTER+DRAFT+SINGLE_INSTANCE_CAPABLE.test.ts
+++ test/integration/draft/singleInstance/AFTER+DRAFT+SINGLE_INSTANCE_CAPABLE.test.ts
@@ -6,12 +6,14 @@ import { Constructable } from '@sap/cds/apis/internal/inference';
 class Customer {
   @AfterRead()
   @SingleInstanceCapable()
+  @Draft()
   public async afterReadMethod(results: any[], req: Request, isSingleInstance: boolean) {}
 
   @AfterCreate()
   @AfterUpdate()
   @AfterDelete()
   @SingleInstanceCapable()
+  @Draft()
   public async afterCreateAndDeleteMethod(results: any[], req: Request, isSingleInstance: boolean) {}
 }
 
@@ -20,6 +22,7 @@ class CustomerWithDraftInBetween {
   @SingleInstanceCapable()
   @AfterCreate()
   @AfterUpdate()
+  @Draft()
   @AfterDelete()
   public async afterReadMethod(results: any[], req: Request, isSingleInstance: boolean) {}
 }
@@ -31,20 +34,23 @@ const decoratorPropsInBetweenDraft = MetadataDispatcher.getMetadataHandlers(newC
 describe('AFTER - Single instance capable', () => {
   describe(`
   @AfterRead()
-  @SingleInstanceCapable()`, () => {
-    it('It should : mark @AfterRead() decorator as "single instance"', async () => {
+  @SingleInstanceCapable()
+  @Draft()`, () => {
+    it('It should : mark @AfterRead() decorator as "single instance" and as a "draft"', () => {
       const foundEvent = decoratorProps.filter((item) => item.event === 'READ')[0];
 
       expect(foundEvent.isSingleInstance).toBe(true);
+      expect(foundEvent.isDraft).toBe(true);
     });
   });
 
   describe(`
   @AfterCreate() 
   @AfterUpdate()
-  @AfterDelete() 
-  @SingleInstanceCapable()`, () => {
-    it('It should : mark @AfterCreate() AND @AfterDelete() decorators as "single instance"', async () => {
+  @AfterDelete()
+  @SingleInstanceCapable()
+  @Draft()`, () => {
+    it('It should : mark @AfterCreate(), @AfterUpdate(), @AfterDelete() decorators as "single instance" and as a "draft"', () => {
       const foundCreate = decoratorProps.filter((item) => item.event === 'CREATE')[0];
       const foundDelete = decoratorProps.filter((item) => item.event === 'DELETE')[0];
       const foundUpdate = decoratorProps.filter((item) => item.event === 'UPDATE')[0];
@@ -54,19 +60,25 @@ describe('AFTER - Single instance capable', () => {
         true,
         true,
       ]);
+
+      expect([foundCreate.isDraft, foundDelete.isDraft, foundUpdate.isDraft]).toStrictEqual([true, true, true]);
     });
   });
 
   describe(`
   @AfterRead()
   @SingleInstanceCapable()
-  @AfterUpdate()
   @AfterCreate()
+  @AfterUpdate()
+  @Draft()
   @AfterDelete()
   `, () => {
-    it('It should : mark @AfterRead() as "single instance" and as NO "single instance" decorators @AfterCreate(), @AfterUpdate(), @AfterDelete()', async () => {
-      const foundRead = decoratorPropsInBetweenDraft.filter((item) => item.event === 'READ')[0];
+    it(`
+    1. It should : mark @AfterRead() as "single instance" and NO "single instance" decorators @AfterCreate(), @AfterUpdate(), @AfterDelete()
+    2. It should : mark as a "draft" decorators  @AfterCreate(), @AfterRead(), @AfterUpdate() and as NO "draft" the decorator @AfterDelete()
+    `, async () => {
       const foundCreate = decoratorPropsInBetweenDraft.filter((item) => item.event === 'CREATE')[0];
+      const foundRead = decoratorPropsInBetweenDraft.filter((item) => item.event === 'READ')[0];
       const foundDelete = decoratorPropsInBetweenDraft.filter((item) => item.event === 'DELETE')[0];
       const foundUpdate = decoratorPropsInBetweenDraft.filter((item) => item.event === 'UPDATE')[0];
 
@@ -76,6 +88,13 @@ describe('AFTER - Single instance capable', () => {
         foundUpdate.isSingleInstance,
         foundDelete.isSingleInstance,
       ]).toStrictEqual([true, undefined, undefined, undefined]);
+
+      expect([foundRead.isDraft, foundCreate.isDraft, foundUpdate.isDraft, foundDelete.isDraft]).toStrictEqual([
+        true,
+        true,
+        true,
+        false,
+      ]);
     });
   });
 });
