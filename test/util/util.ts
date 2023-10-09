89f36c384f67799584d9676b58d390b0b45af9d9
dragolea <dragolea@yahoo.com>
2023-10-09 17:41:13 +0300

    Update util.ts

--- test/util/util.ts
+++ test/util/util.ts
@@ -1,16 +1,7 @@
 import path from 'path';
 import cds from '@sap/cds';
 
-export const connectTest = (dirName: string, projectName: string) => {
-  const project = path.join(dirName, '..', projectName);
-  cds.test(project);
-
-  return cds;
-};
-
-export const getBooksEntity = async (_cds: ReturnType<typeof connectTest>) => {
-  const CatalogService = await _cds.connect.to('CatalogService');
-  const { Books } = CatalogService.entities;
-
-  return Books;
+export const connectTest = (dirName: string) => {
+  const project = path.join(dirName, '..', 'bookshop');
+  return cds.test(project);
 };
