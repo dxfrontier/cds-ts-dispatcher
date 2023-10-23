c0980a24703ec33a4b45fee56c6c82b4b76b2c49
dragolea <dragolea@yahoo.com>
2023-10-23 11:51:36 +0300

    Update index.ts
    Exported type TypedRequest, type ActionRequest, type ActionReturn

--- lib/index.ts
+++ lib/index.ts
@@ -3,7 +3,7 @@ import { inject as Inject } from 'inversify';
 
 export * from './decorators/class';
 export * from './decorators/method';
-export { ServiceHelper } from './util/types/types';
+export { ServiceHelper, type TypedRequest, type ActionRequest, type ActionReturn } from './util/types/types';
 export * from './util/helpers/CDSDispatcher';
 
 export { Inject };
