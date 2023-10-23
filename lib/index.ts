031c81e4b9c337f12288381dc2049427f7790b9c
dragolea <dragolea@yahoo.com>
2023-10-23 13:11:22 +0300

    Changes to package.json & small change to lib/index.ts

--- lib/index.ts
+++ lib/index.ts
@@ -1,9 +1,7 @@
-// Imported and exported to Uppercase to be in guidance with other decorators.
-import { inject as Inject } from 'inversify';
-
 export * from './decorators/class';
 export * from './decorators/method';
 export { ServiceHelper, type TypedRequest, type ActionRequest, type ActionReturn } from './util/types/types';
 export * from './util/helpers/CDSDispatcher';
 
-export { Inject };
+// Exported to uppercase to be in guidance with other decorators.
+export { inject as Inject } from 'inversify';
