3b7fc651f20c60223435c6f96d03526961cddd36
dragolea <dragolea@yahoo.com>
2024-01-22 09:42:26 +0200

    Deleted: tests file

--- test/bookshop/@cds-models/index.js
+++
@@ -1,8 +0,0 @@
-// This is an automatically generated file. Please do not change its contents manually!
-const cds = require('@sap/cds')
-const csn = cds.entities('')
-// events
-// actions
-// enums
-module.exports.Roles ??= { USER: "USER", ADMIN: "ADMIN" }
-module.exports.BookTypes ??= { BOOK_SIGNING: "BOOK_SIGNING", AUTHOR_TALK: "AUTHOR_TALK", BOOK_LUNCH: "BOOK_LUNCH" }
