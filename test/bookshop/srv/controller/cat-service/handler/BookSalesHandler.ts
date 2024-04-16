a42e48c4a2dc350e13fc5c3efe64a44187a2e2ce
dragolea <dragolea@yahoo.com>
2024-04-16 19:17:08 +0300

    update: tests

--- test/bookshop/srv/controller/cat-service/handler/BookSalesHandler.ts
+++ test/bookshop/srv/controller/cat-service/handler/BookSalesHandler.ts
@@ -16,34 +16,22 @@ import {
   Req,
   Request,
   Results,
+  RoleSpecificLogic,
   Service,
   SingleInstanceSwitch,
   SRV,
 } from '../../../../../../lib';
 import BookSalesService from '../../../service/BookSalesService';
+import { ScoopedUserLogic } from '../../../user/ScoopedUserLogic';
 
 @EntityHandler(BookSale)
 class BookSalesHandler {
   @Inject(SRV) private readonly srv: Service;
   @Inject(BookSalesService) private readonly bookSalesService: BookSalesService;
 
-  // *******************************************************
-
-  /* TODO: execute an event only when role is found in the req.user
-   1. 
-   Overloads:
-   1. @ScopedUserLogic('role', handleClass) => Execute some logic, like some other events (@AfterRead ... etc)
-   2. @ScopedUserLogic('role') => execute logic only (event + decorators) for and ONLY for the user defined in the @ScopedUserLogic
-   *********************************************************
-   2. new decorator @JwtFromDestination(destination?: string), which should look in a destination and get the JWT token from that designation ... 
-      currently we support only @Jwt from the current Request 
-
-  */
-  // *******************************************************
-
   @AfterRead()
   @ExecutionAllowedForRole('Manager', 'User', 'CEO')
-  // @RoleSpecificLogic('Manager', ScoopedUserLogic)
+  @RoleSpecificLogic('Manager', ScoopedUserLogic)
   private async afterRead(
     @IsColumnSupplied<BookSale>('quantity') hasQuantity: boolean,
     @IsColumnSupplied<BookSale>('saleDate') hasSaleDate: boolean,
@@ -64,7 +52,7 @@ class BookSalesHandler {
 
     @SingleInstanceSwitch() isSingleInstance: boolean,
 
-    // @Jwt() token: string | undefined,
+    @Jwt() token: string | undefined,
   ) {
     if (roles) {
       req.notify('Manager');
@@ -77,9 +65,9 @@ class BookSalesHandler {
       req.notify('Entity set');
     }
 
-    // if (token) {
-    //   req.notify(token);
-    // }
+    if (token) {
+      req.notify(token);
+    }
 
     if (locale) {
       req.notify('locale');
