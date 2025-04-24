ee65b5804bcb236802c60d58c0f6b9913abc7591
dragolea <dragolea@yahoo.com>
2025-04-24 16:08:48 +0300

    chore(prettier): pretty test folder

--- test/sample-project/monorepo_bookshop/services/admin/db/@cds-models/index.js
+++ test/sample-project/monorepo_bookshop/services/admin/db/@cds-models/index.js
@@ -1,12 +1,12 @@
 // This is an automatically generated file. Please do not change its contents manually!
-const cds = require('@sap/cds')
-const csn = cds.entities('')
+const cds = require('@sap/cds');
+const csn = cds.entities('');
 // HelloRequest
-module.exports.HelloRequest = { is_singular: true, __proto__: csn.HelloRequest }
+module.exports.HelloRequest = { is_singular: true, __proto__: csn.HelloRequest };
 // HelloResponse
-module.exports.HelloResponse = { is_singular: true, __proto__: csn.HelloResponse }
+module.exports.HelloResponse = { is_singular: true, __proto__: csn.HelloResponse };
 // events
 // actions
 // enums
-module.exports.Roles ??= { USER: "USER", ADMIN: "ADMIN" }
-module.exports.BookTypes ??= { BOOK_SIGNING: "BOOK_SIGNING", AUTHOR_TALK: "AUTHOR_TALK", BOOK_LUNCH: "BOOK_LUNCH" }
+module.exports.Roles ??= { USER: 'USER', ADMIN: 'ADMIN' };
+module.exports.BookTypes ??= { BOOK_SIGNING: 'BOOK_SIGNING', AUTHOR_TALK: 'AUTHOR_TALK', BOOK_LUNCH: 'BOOK_LUNCH' };
--- test/sample-project/monorepo_bookshop/services/admin/db/@cds-models/sap/capire/bookshop/index.js
+++ test/sample-project/monorepo_bookshop/services/admin/db/@cds-models/sap/capire/bookshop/index.js
@@ -1,54 +1,54 @@
 // This is an automatically generated file. Please do not change its contents manually!
-const cds = require('@sap/cds')
-const csn = cds.entities('sap.capire.bookshop')
+const cds = require('@sap/cds');
+const csn = cds.entities('sap.capire.bookshop');
 // Books
-module.exports.Book = { is_singular: true, __proto__: csn.Books }
-module.exports.Books = csn.Books
+module.exports.Book = { is_singular: true, __proto__: csn.Books };
+module.exports.Books = csn.Books;
 // Publishers
-module.exports.Publisher = { is_singular: true, __proto__: csn.Publishers }
-module.exports.Publishers = csn.Publishers
+module.exports.Publisher = { is_singular: true, __proto__: csn.Publishers };
+module.exports.Publishers = csn.Publishers;
 // BookStats
-module.exports.BookStat = { is_singular: true, __proto__: csn.BookStats }
-module.exports.BookStats = csn.BookStats
+module.exports.BookStat = { is_singular: true, __proto__: csn.BookStats };
+module.exports.BookStats = csn.BookStats;
 // Authors
-module.exports.Author = { is_singular: true, __proto__: csn.Authors }
-module.exports.Authors = csn.Authors
+module.exports.Author = { is_singular: true, __proto__: csn.Authors };
+module.exports.Authors = csn.Authors;
 // Genres
-module.exports.Genre = { is_singular: true, __proto__: csn.Genres }
-module.exports.Genres = csn.Genres
+module.exports.Genre = { is_singular: true, __proto__: csn.Genres };
+module.exports.Genres = csn.Genres;
 // Reviews
-module.exports.Review = { is_singular: true, __proto__: csn.Reviews }
-module.exports.Reviews = csn.Reviews
+module.exports.Review = { is_singular: true, __proto__: csn.Reviews };
+module.exports.Reviews = csn.Reviews;
 // BookEvents
-module.exports.BookEvent = { is_singular: true, __proto__: csn.BookEvents }
-module.exports.BookEvents = csn.BookEvents
+module.exports.BookEvent = { is_singular: true, __proto__: csn.BookEvents };
+module.exports.BookEvents = csn.BookEvents;
 // BookSales
-module.exports.BookSale = { is_singular: true, __proto__: csn.BookSales }
-module.exports.BookSales = csn.BookSales
+module.exports.BookSale = { is_singular: true, __proto__: csn.BookSales };
+module.exports.BookSales = csn.BookSales;
 // Users
-module.exports.User = { is_singular: true, __proto__: csn.Users }
-module.exports.Users = csn.Users
+module.exports.User = { is_singular: true, __proto__: csn.Users };
+module.exports.Users = csn.Users;
 // UserActivityLog
-module.exports.UserActivityLog = { is_singular: true, __proto__: csn.UserActivityLog }
-module.exports.UserActivityLog_ = csn.UserActivityLog
+module.exports.UserActivityLog = { is_singular: true, __proto__: csn.UserActivityLog };
+module.exports.UserActivityLog_ = csn.UserActivityLog;
 // Promotions
-module.exports.Promotion = { is_singular: true, __proto__: csn.Promotions }
-module.exports.Promotions = csn.Promotions
+module.exports.Promotion = { is_singular: true, __proto__: csn.Promotions };
+module.exports.Promotions = csn.Promotions;
 // BookOrders
-module.exports.BookOrder = { is_singular: true, __proto__: csn.BookOrders }
-module.exports.BookOrders = csn.BookOrders
+module.exports.BookOrder = { is_singular: true, __proto__: csn.BookOrders };
+module.exports.BookOrders = csn.BookOrders;
 // BookRecommendations
-module.exports.BookRecommendation = { is_singular: true, __proto__: csn.BookRecommendations }
-module.exports.BookRecommendations = csn.BookRecommendations
+module.exports.BookRecommendation = { is_singular: true, __proto__: csn.BookRecommendations };
+module.exports.BookRecommendations = csn.BookRecommendations;
 // BookFormats
-module.exports.BookFormat = { is_singular: true, __proto__: csn.BookFormats }
-module.exports.BookFormats = csn.BookFormats
+module.exports.BookFormat = { is_singular: true, __proto__: csn.BookFormats };
+module.exports.BookFormats = csn.BookFormats;
 // Books.texts
-module.exports.Books.text = { is_singular: true, __proto__: csn['Books.texts'] }
-module.exports.Books.texts = csn['Books.texts']
+module.exports.Books.text = { is_singular: true, __proto__: csn['Books.texts'] };
+module.exports.Books.texts = csn['Books.texts'];
 // Genres.texts
-module.exports.Genres.text = { is_singular: true, __proto__: csn['Genres.texts'] }
-module.exports.Genres.texts = csn['Genres.texts']
+module.exports.Genres.text = { is_singular: true, __proto__: csn['Genres.texts'] };
+module.exports.Genres.texts = csn['Genres.texts'];
 // events
 // actions
 // enums
