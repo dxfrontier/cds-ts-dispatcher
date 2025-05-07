e06f636982c80c5e956cc23619de3135e761db6e
dragolea <dragolea@yahoo.com>
2025-05-07 10:02:48 +0300

    chore(postinstall): prettier

--- postinstall/PostInstall.js
+++ postinstall/PostInstall.js
@@ -1,51 +1,57 @@
-"use strict";
+'use strict';
 var __create = Object.create;
 var __defProp = Object.defineProperty;
 var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
 var __getOwnPropNames = Object.getOwnPropertyNames;
 var __getProtoOf = Object.getPrototypeOf;
 var __hasOwnProp = Object.prototype.hasOwnProperty;
-var __name = (target, value) => __defProp(target, "name", { value, configurable: true });
+var __name = (target, value) => __defProp(target, 'name', { value, configurable: true });
 var __copyProps = (to, from, except, desc) => {
-  if (from && typeof from === "object" || typeof from === "function") {
+  if ((from && typeof from === 'object') || typeof from === 'function') {
     for (let key of __getOwnPropNames(from))
       if (!__hasOwnProp.call(to, key) && key !== except)
-        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
+        __defProp(to, key, {
+          get: () => from[key],
+          enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable,
+        });
   }
   return to;
 };
-var __toESM = (mod, isNodeMode, target) => (target = mod != null ? __create(__getProtoOf(mod)) : {}, __copyProps(
-  // If the importer is in node compatibility mode or this is not an ESM
-  // file that has been converted to a CommonJS file using a Babel-
-  // compatible transform (i.e. "__esModule" has not been set), then set
-  // "default" to the CommonJS "module.exports" for node compatibility.
-  isNodeMode || !mod || !mod.__esModule ? __defProp(target, "default", { value: mod, enumerable: true }) : target,
-  mod
-));
+var __toESM = (mod, isNodeMode, target) => (
+  (target = mod != null ? __create(__getProtoOf(mod)) : {}),
+  __copyProps(
+    // If the importer is in node compatibility mode or this is not an ESM
+    // file that has been converted to a CommonJS file using a Babel-
+    // compatible transform (i.e. "__esModule" has not been set), then set
+    // "default" to the CommonJS "module.exports" for node compatibility.
+    isNodeMode || !mod || !mod.__esModule ? __defProp(target, 'default', { value: mod, enumerable: true }) : target,
+    mod,
+  )
+);
 
 // postinstall/util/EnvGenerator.ts
-var import_fs2 = require("fs");
-var import_json_ts = require("json-ts");
+var import_fs2 = require('fs');
+var import_json_ts = require('json-ts');
 
 // postinstall/util/FileManager.ts
-var import_path = __toESM(require("path"));
-var import_fs = require("fs");
-var import_jsonc_parser = require("jsonc-parser");
-var import_fast_glob = __toESM(require("fast-glob"));
+var import_path = __toESM(require('path'));
+var import_fs = require('fs');
+var import_jsonc_parser = require('jsonc-parser');
+var import_fast_glob = __toESM(require('fast-glob'));
 var FileManager = class _FileManager {
   static {
-    __name(this, "FileManager");
+    __name(this, 'FileManager');
   }
   currentInstallDirectory = process.env.INIT_CWD;
   dispatcherNecessaryFiles = {
-    packageJson: "package.json",
-    folder: "@dispatcher",
-    env: "index.ts",
-    gitIgnore: ".gitignore",
-    tsConfig: "tsconfig.json"
+    packageJson: 'package.json',
+    folder: '@dispatcher',
+    env: 'index.ts',
+    gitIgnore: '.gitignore',
+    tsConfig: 'tsconfig.json',
   };
   dispatcherExecutionPath = {
-    paths: []
+    paths: [],
   };
   constructor() {
     this.run();
@@ -54,75 +60,78 @@ var FileManager = class _FileManager {
     return import_path.default.join(...paths);
   }
   readPackageJson(filePath) {
-    return JSON.parse((0, import_fs.readFileSync)(filePath, "utf8"));
+    return JSON.parse((0, import_fs.readFileSync)(filePath, 'utf8'));
   }
   getRootPackageJson() {
     const path2 = _FileManager.joinPaths(this.currentInstallDirectory, this.dispatcherNecessaryFiles.packageJson);
     const packageJson = this.readPackageJson(path2);
     return {
-      hasWorkspaces: /* @__PURE__ */ __name(() => (packageJson.workspaces?.length ?? 0) > 0, "hasWorkspaces"),
-      getWorkspaces: /* @__PURE__ */ __name(() => packageJson.workspaces, "getWorkspaces")
+      hasWorkspaces: /* @__PURE__ */ __name(() => (packageJson.workspaces?.length ?? 0) > 0, 'hasWorkspaces'),
+      getWorkspaces: /* @__PURE__ */ __name(() => packageJson.workspaces, 'getWorkspaces'),
     };
   }
   isWorkspaceDynamicPattern(workspace) {
     return import_fast_glob.default.isDynamicPattern(workspace) ? true : false;
   }
   getParsedPackageJson(workspace) {
-    const sanitizedWorkspace = workspace.replace("*", "");
+    const sanitizedWorkspace = workspace.replace('*', '');
     const path2 = _FileManager.joinPaths(this.currentInstallDirectory, sanitizedWorkspace);
     const resolveRoot = /* @__PURE__ */ __name(() => {
       return _FileManager.joinPaths(this.currentInstallDirectory, this.dispatcherNecessaryFiles.packageJson);
-    }, "resolveRoot");
+    }, 'resolveRoot');
     const resolveDynamicPattern = /* @__PURE__ */ __name(() => {
       const workspaces = import_fast_glob.default.globSync(`${path2}*/${this.dispatcherNecessaryFiles.packageJson}`, {
-        dot: true
+        dot: true,
       });
       const jsons = [];
       workspaces.forEach((item, index) => {
         const fields = {
-          path: item.replace("package.json", ""),
-          ...JSON.parse((0, import_fs.readFileSync)(workspaces[index], "utf8"))
+          path: item.replace('package.json', ''),
+          ...JSON.parse((0, import_fs.readFileSync)(workspaces[index], 'utf8')),
         };
         jsons.push(fields);
       });
       return jsons;
-    }, "resolveDynamicPattern");
+    }, 'resolveDynamicPattern');
     const resolveStaticWorkspaces = /* @__PURE__ */ __name(() => {
       const fields = {
         path: path2,
-        ...JSON.parse((0, import_fs.readFileSync)(_FileManager.joinPaths(path2, this.dispatcherNecessaryFiles.packageJson), "utf8"))
+        ...JSON.parse(
+          (0, import_fs.readFileSync)(_FileManager.joinPaths(path2, this.dispatcherNecessaryFiles.packageJson), 'utf8'),
+        ),
       };
-      return [
-        fields
-      ];
-    }, "resolveStaticWorkspaces");
+      return [fields];
+    }, 'resolveStaticWorkspaces');
     return {
       resolveDynamicPattern,
       resolveStaticWorkspaces,
-      resolveRoot
+      resolveRoot,
     };
   }
   createFolderIfAbsent(folderPath) {
     if (!(0, import_fs.existsSync)(folderPath)) {
       (0, import_fs.mkdirSync)(folderPath, {
-        recursive: true
+        recursive: true,
       });
     }
   }
-  createFileIfAbsent(filePath, defaultContent = "") {
+  createFileIfAbsent(filePath, defaultContent = '') {
     if (!(0, import_fs.existsSync)(filePath)) {
       (0, import_fs.writeFileSync)(filePath, defaultContent);
     }
   }
   validateDispatcherDependency(dependencies) {
-    return dependencies && dependencies["@dxfrontier/cds-ts-dispatcher"] !== void 0;
+    return dependencies && dependencies['@dxfrontier/cds-ts-dispatcher'] !== void 0;
   }
   appendLineIfAbsent(filePath, line) {
-    const content = (0, import_fs.readFileSync)(filePath, "utf8");
+    const content = (0, import_fs.readFileSync)(filePath, 'utf8');
     if (!content.includes(line)) {
-      (0, import_fs.appendFileSync)(filePath, `
+      (0, import_fs.appendFileSync)(
+        filePath,
+        `
 ${line}
-`);
+`,
+      );
     }
   }
   updateGitIgnore(filePath) {
@@ -130,25 +139,25 @@ ${line}
   }
   updatePackageJsonImports(filePath) {
     if ((0, import_fs.existsSync)(filePath)) {
-      const json = JSON.parse((0, import_fs.readFileSync)(filePath, "utf8"));
+      const json = JSON.parse((0, import_fs.readFileSync)(filePath, 'utf8'));
       json.imports = json.imports || {};
-      if (!json.imports["#dispatcher"]) {
-        json.imports["#dispatcher"] = "./@dispatcher/index.js";
+      if (!json.imports['#dispatcher']) {
+        json.imports['#dispatcher'] = './@dispatcher/index.js';
         (0, import_fs.writeFileSync)(filePath, JSON.stringify(json, null, 2));
       }
     }
   }
   updateTsconfigInclude(filePath) {
     if ((0, import_fs.existsSync)(filePath)) {
-      const tsconfigContent = (0, import_fs.readFileSync)(filePath, "utf8");
+      const tsconfigContent = (0, import_fs.readFileSync)(filePath, 'utf8');
       const errors = [];
       const tsconfig = (0, import_jsonc_parser.parse)(tsconfigContent, errors);
       if (errors.length > 0) {
-        throw new Error("tsconfig.json contains comments or invalid JSON format !");
+        throw new Error('tsconfig.json contains comments or invalid JSON format !');
       }
       tsconfig.include = tsconfig.include || [];
-      if (!tsconfig.include.includes("./@dispatcher")) {
-        tsconfig.include.push("./@dispatcher");
+      if (!tsconfig.include.includes('./@dispatcher')) {
+        tsconfig.include.push('./@dispatcher');
         (0, import_fs.writeFileSync)(filePath, JSON.stringify(tsconfig, null, 2));
       }
     }
@@ -172,12 +181,12 @@ ${line}
       envFilePath,
       gitignoreFilePath,
       packageJsonPath,
-      tsconfigPath
+      tsconfigPath,
     });
     this.dispatcherExecutionPath.paths.push({
       executedInstalledPath: directory,
       envFilePath,
-      dispatcherPath: dispatcherFolderPath
+      dispatcherPath: dispatcherFolderPath,
     });
   }
   processRoot() {
@@ -187,10 +196,14 @@ ${line}
     const workspaces = this.getRootPackageJson().getWorkspaces();
     workspaces.forEach((workspace) => {
       const isDynamic = this.isWorkspaceDynamicPattern(workspace);
-      const packages = isDynamic ? this.getParsedPackageJson(workspace).resolveDynamicPattern() : this.getParsedPackageJson(workspace).resolveStaticWorkspaces();
+      const packages = isDynamic
+        ? this.getParsedPackageJson(workspace).resolveDynamicPattern()
+        : this.getParsedPackageJson(workspace).resolveStaticWorkspaces();
       packages.forEach((pkg) => {
         if (this.validateDispatcherDependency(pkg.dependencies)) {
-          isDynamic ? this.processInstallation(pkg.path) : this.processInstallation(_FileManager.joinPaths(this.currentInstallDirectory, workspace));
+          isDynamic
+            ? this.processInstallation(pkg.path)
+            : this.processInstallation(_FileManager.joinPaths(this.currentInstallDirectory, workspace));
         }
       });
     });
@@ -205,17 +218,16 @@ ${line}
 };
 
 // postinstall/util/ShellCommander.ts
-var import_cross_spawn = require("cross-spawn");
+var import_cross_spawn = require('cross-spawn');
 var ShellCommander = class {
   static {
-    __name(this, "ShellCommander");
+    __name(this, 'ShellCommander');
   }
-  constructor() {
-  }
+  constructor() {}
   executeCommand(command, args, currentExecutionPath) {
     const result = (0, import_cross_spawn.sync)(command, args, {
-      encoding: "utf8",
-      cwd: currentExecutionPath
+      encoding: 'utf8',
+      cwd: currentExecutionPath,
     });
     if (result.error && result.stderr) {
       throw new Error(result.stderr);
@@ -223,32 +235,25 @@ var ShellCommander = class {
     return result.stdout;
   }
   compileEnvFile(envFilePath, dispatcherFolderPath) {
-    this.executeCommand("npx tsc", [
-      envFilePath,
-      "--outDir",
-      dispatcherFolderPath
-    ]);
+    this.executeCommand('npx tsc', [envFilePath, '--outDir', dispatcherFolderPath]);
   }
 };
 
 // postinstall/util/EnvGenerator.ts
 var EnvGenerator = class {
   static {
-    __name(this, "EnvGenerator");
+    __name(this, 'EnvGenerator');
   }
   fileManager = new FileManager();
   shellCommander = new ShellCommander();
   generateTypeDefinitions(jsonString) {
     return (0, import_json_ts.json2ts)(jsonString, {
-      rootName: "CDS_ENV",
-      prefix: ""
+      rootName: 'CDS_ENV',
+      prefix: '',
     });
   }
   getCdsEnvOutput(path2) {
-    return this.shellCommander.executeCommand("cds", [
-      "env",
-      "get"
-    ], path2);
+    return this.shellCommander.executeCommand('cds', ['env', 'get'], path2);
   }
   createEnvFile(filePath, envConfig) {
     const typeDefinitions = this.generateTypeDefinitions(envConfig);
@@ -270,7 +275,7 @@ export ${typeDefinitions}`;
     try {
       this.generateEnvFiles();
     } catch (error) {
-      console.error("Error generating environment files:", error);
+      console.error('Error generating environment files:', error);
       process.exit(1);
     }
   }
@@ -279,7 +284,7 @@ export ${typeDefinitions}`;
 // postinstall/PostInstall.ts
 var PostInstall = class PostInstall2 {
   static {
-    __name(this, "PostInstall");
+    __name(this, 'PostInstall');
   }
   GenerateEnv;
   run() {
