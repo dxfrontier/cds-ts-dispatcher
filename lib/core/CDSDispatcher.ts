6497eaeb4b4b828bac2baaa8f4b8c499a3f96aaa
dragolea <dragolea@yahoo.com>
2025-08-20 09:27:38 +0300

    refactor(receiverMessage): removed testing object from the CDSDispatcher

--- lib/core/CDSDispatcher.ts
+++ lib/core/CDSDispatcher.ts
@@ -329,18 +329,6 @@ class CDSDispatcher {
           const text = `> received: ${eventName}`;
 
           console.log(util.showGreenConsole(text));
-          req.data = {
-            bla: {
-              caca: 'dada',
-              age: 'dadad',
-            },
-            res: {
-              eaaa: {
-                caca: 'dadada',
-              },
-            },
-            test: '3232',
-          };
           console.table(...[req.data]);
         } else {
           const text = `> received: ${eventName}`;
