762464a715e62ee9c429053ed72bc678d2a4ba0d
dragolea <dragolea@yahoo.com>
2025-08-26 14:03:07 +0300

    chore(prettier): prettied files

--- test/sample-project/monorepo_bookshop/services/admin/@cds-models/cds/outbox/index.js
+++ test/sample-project/monorepo_bookshop/services/admin/@cds-models/cds/outbox/index.js
@@ -1,8 +1,8 @@
 // This is an automatically generated file. Please do not change its contents manually!
-const { createEntityProxy } = require('./../../_')
+const { createEntityProxy } = require('./../../_');
 // Messages
-module.exports.Message = createEntityProxy(['cds.outbox', 'Messages'], { target: { is_singular: true } })
-module.exports.Messages = createEntityProxy(['cds.outbox', 'Messages'], { target: { is_singular: false }})
+module.exports.Message = createEntityProxy(['cds.outbox', 'Messages'], { target: { is_singular: true } });
+module.exports.Messages = createEntityProxy(['cds.outbox', 'Messages'], { target: { is_singular: false } });
 // events
 // actions
 // enums
